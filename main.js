3aa45a71ec649e373272045d01e28359292a05bd
floydcal <160587879+floydcal@users.noreply.github.com>
2024-03-30 16:40:19 +0500

    inputToDo

---
+++ main.js
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([{
+            name: "todos",
+            type: "input",
+            message: "What do you want to add to your todos?"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more?",
+            default: "false",
+        }]);
+    todos.push(addTask.todos);
+    condition = addTask.addMore;
+    console.log(todos);
+}
